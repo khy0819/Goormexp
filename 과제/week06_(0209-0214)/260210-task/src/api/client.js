392f7acef2521001d9e629b57816cb5bae3efba3
김혜은 <78162567+khy0819@users.noreply.github.com>
2026-02-11 17:08:03 +0900

    Refactor

--- 과제/week06_(0209-0214)/260210-task/src/api/client.js
+++ 과제/week06_(0209-0214)/260210-task/src/api/client.js
@@ -1,7 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 
 const client = axios.create({
   baseURL: "https://54.180.25.65:3002/api", // .env 파일에서 불러온 값 사용
 });
 
 export default client;
+//
