cb14b4b44441937646b4c509fbbdc17fd20907a0
김혜은 <78162567+khy0819@users.noreply.github.com>
2026-02-11 16:48:14 +0900

    client.js 파일 수정: baseURL에서 http 프로토콜 제거

--- 과제/week06_(0209-0214)/260210-task/src/api/client.js
+++ 과제/week06_(0209-0214)/260210-task/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://54.180.25.65:3002/api", // .env 파일에서 불러온 값 사용
+  baseURL: "54.180.25.65:3002/api", // .env 파일에서 불러온 값 사용
 });
 
 export default client;
