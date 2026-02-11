4c1930d227f50167f0222499ad6d332b6bc511ba
김혜은 <78162567+khy0819@users.noreply.github.com>
2026-02-11 16:32:32 +0900

    client.js 파일 수정: baseURL 프로토콜을 http로 변경

--- 과제/week06_(0209-0214)/260210-task/src/api/client.js
+++ 과제/week06_(0209-0214)/260210-task/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://localhost:3002/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3002/api",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
