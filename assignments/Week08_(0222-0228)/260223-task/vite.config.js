e70dc2edbde415cae371e4a517fd4e1a3896ff43
김혜은 <78162567+khy0819@users.noreply.github.com>
2026-03-04 15:02:42 +0900

    fix: correct base path

--- assignments/Week08_(0222-0228)/260223-task/vite.config.js
+++ assignments/Week08_(0222-0228)/260223-task/vite.config.js
@@ -3,15 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Goormexp/assignments/23-react-app/dist/",
-
-  server: {
-    port: 3002,
-    proxy: {
-      "/api": {
-        target: "http://54.180.25.65:3002",
-        changeOrigin: true,
-      },
-    },
-  },
+  base: "./", // ← 이것만 쓰면 된다
 });
