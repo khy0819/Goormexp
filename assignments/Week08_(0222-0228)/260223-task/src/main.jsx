737fa93f2ecfac8787e45ae5b44fc0054f0ba8b8
김혜은 <78162567+khy0819@users.noreply.github.com>
2026-03-04 16:27:39 +0900

    라우터 수정

--- assignments/Week08_(0222-0228)/260223-task/src/main.jsx
+++ assignments/Week08_(0222-0228)/260223-task/src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./App.css";
+import "./index.css";
+import { HashRouter } from "react-router-dom"; // 👉 브라우저라우터 대신 해시라우터 불러오기
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    {/* basename은 vite.config.js의 base와 일치시켜야 합니다 */}
-    <BrowserRouter>
-      {" "}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </HashRouter>
+  </React.StrictMode>,
 );
