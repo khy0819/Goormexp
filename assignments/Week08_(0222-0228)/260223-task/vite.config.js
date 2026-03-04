d719af59554fefa39a2d0b1b203f140f28cdb478
김혜은 <78162567+khy0819@users.noreply.github.com>
2026-03-04 14:57:24 +0900

    feat: package.json에 homepage 추가 및 Vite 설정의 base 경로 수정, index.html에서 CSS 주석 정리 및 GitHub 링크 수정

--- assignments/Week08_(0222-0228)/260223-task/vite.config.js
+++ assignments/Week08_(0222-0228)/260223-task/vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/Goormexp/assignments/23-react-app/dist/",
+
   server: {
     port: 3002,
     proxy: {
